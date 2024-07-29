a49e679a0ca5950ee63493bf03a5d7a9e3dfc2f0
sufiyanTheDev <sufiyangafoordeveloper@outllok.com>
2024-07-29 06:31:34 +0530

    typescript rollup build configuration

---
+++ packages/react/rollup.config.mjs
@@ -0,0 +1,16 @@
+import Ts from 'rollup-plugin-typescript2';
+
+export default {
+    input: [
+        'src/index.ts',
+        'src/atoms/Button/index.ts'
+    ],
+    output: {
+        dir: 'lib',
+        format: 'esm',
+        sourcemap: true,
+        preserveModules: true,
+    },
+    plugins: [Ts()],
+    external: ['react']
+}
